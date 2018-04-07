1ab65e36da7a9b1a83c3a57892c56065b2278958
umardani <umardani077@gmail.com>
2018-04-08 00:04:15 +0800

    buat method cetakInfo()

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -3,4 +3,10 @@ public class Sepeda {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t:"+merk+"\n"+
+                "Warna \t:"+warna+"\n"+
+                "Harga \t:"+harga);
+    } 
 }
