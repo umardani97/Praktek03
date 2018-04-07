8b0d3f598e9ce1e974cf113031a7343e984d97fc
umardani <umardani077@gmail.com>
2018-04-08 00:55:11 +0800

    Gettering dan Settering

--- src/praktek03/SepedaGunung.java
+++ src/praktek03/SepedaGunung.java
@@ -1,4 +1,18 @@
 package praktek03;
 public class SepedaGunung extends Sepeda{
+    private String Jenisban;
+    
+    void trottle(){
+        System.out.println("Bawa Aman aja brayy");
+    }
+
+    public String getJenisban() {
+        return Jenisban;
+    }
+
+    public void setJenisban(String Jenisban) {
+        this.Jenisban = Jenisban;
+    }
+    
     
 }
