f25ff820abe5d50193e7a0b5699ce1444c6a9013
umardani <umardani077@gmail.com>
2018-04-07 23:59:04 +0800

    membuat atribut

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
-
-/**
- *
- * @author JKendil
- */
 public class Sepeda {
-    
+    String merk;
+    String warna;
+    double harga;
 }
