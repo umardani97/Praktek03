bcde9d119b44b9abd9c0b9b46dae60c75600cce2
umardani <umardani077@gmail.com>
2018-04-08 00:14:25 +0800

    Instance buat atribut dan panggil method

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -1,4 +1,11 @@
 package praktek03;
 public class SepedaAksi {
-    
+    public static void main(String[] args) {
+        Sepeda a = new Sepeda();
+        a.merk="Lion";
+        a.warna="Merah";
+        a.harga=12000;
+        
+        a.cetakInfo();
+    }
 }
