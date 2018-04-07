58f4ac99af16fffde718b256ab49844d6cf480de
umardani <umardani077@gmail.com>
2018-04-08 00:20:06 +0800

    menggunakan method set

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -2,10 +2,10 @@ package praktek03;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda a = new Sepeda();
-        a.merk="Lion";
-        a.warna="Merah";
-        a.harga=12000;
-        
-        a.cetakInfo();
+       a.setMerk("Lion");
+       a.setWarna("Merah");
+       a.setHarga(12000);
+       
+       a.cetakInfo();
     }
 }
