84b3e304fb5bdc164d97c49abd73686ca21a3449
umardani <umardani077@gmail.com>
2018-04-08 00:31:52 +0800

    atribut dan method

--- src/praktek03/SepedaMotor.java
+++ src/praktek03/SepedaMotor.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
-
-/**
- *
- * @author JKendil
- */
-public class SepedaMotor {
+public class SepedaMotor extends Sepeda {
+    private String jenismotor;
     
+    void throttle(){
+        System.out.println("BOOOM....");
+    }
 }
