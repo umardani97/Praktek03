4e894cfdc8eab492755cd4ab682a994f34f77504
umardani <umardani077@gmail.com>
2018-04-07 23:57:50 +0800

    membuat class sepeda

--- src/praktek03/Praktek03.java
+++ src/praktek03/Praktek03.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
-
-/**
- *
- * @author JKendil
- */
 public class Praktek03 {
 
     /**
