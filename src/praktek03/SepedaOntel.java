39c22722e50785d7bace5d1648325fa8b5bcc3ba
umardani <umardani077@gmail.com>
2018-04-08 00:47:53 +0800

    Gettering and settering

--- src/praktek03/SepedaOntel.java
+++ src/praktek03/SepedaOntel.java
@@ -1,4 +1,18 @@
 package praktek03;
 public class SepedaOntel extends Sepeda {
+    private String jenisontel;
+    
+    void throttle(){
+        System.out.println("Santay Bae bro..");
+    }
+
+    public String getJenisontel() {
+        return jenisontel;
+    }
+
+    public void setJenisontel(String jenisontel) {
+        this.jenisontel = jenisontel;
+    }
+    
     
 }
